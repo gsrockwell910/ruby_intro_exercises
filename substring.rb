9f72e1a0c520ee8c8d607137ab8cfef467b57e89
Gavin Rockwell <gsrockwell910@gmail.com>
2023-10-15 23:09:19 -0400

    Remove some comments

--- substring.rb
+++ substring.rb
@@ -3,18 +3,16 @@ dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","o
 
 #create substring method
 def substring(string, dictionary) 
-  #turn string into array and transform to lowercase
+  #turn string into array and lowercase
   string_array = string.downcase!.split
 
-  #reduce array down to a hash object with 
+  #reduce array down to a hash object 
   string_array.reduce(Hash.new(0)) do |hash|
     #look at each word in the defined dictionary
     dictionary.each do |word|
-      #if user given string contains a word in the defined dictionary set hash key to that word,
-      #and value to the number of matches returned by string#scan(word)#count. if not move on
       string.include?(word) ? hash[word] = string.scan(word).count : next
     end
-    #return hash (implicit)
+    #return hash
     hash
   end
 end
